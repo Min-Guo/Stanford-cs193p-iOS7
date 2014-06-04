01f78916a8ffd6b40096df0b879f72f7944cf356
Min Guo <xmguomin@gmail.com>
2014-06-04 11:32:03 -0400

    Add Matchismo

---
+++ Matchismo/Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by min guo on 6/4/14.
+//  Copyright (c) 2014 CS193p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ Matchismo/Matchismo/CardGameViewController.m
@@ -0,0 +1,29 @@
+//
+//  CardGameViewController.m
+//  Matchismo
+//
+//  Created by min guo on 6/4/14.
+//  Copyright (c) 2014 CS193p. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface CardGameViewController ()
+
+@end
+
+@implementation CardGameViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
